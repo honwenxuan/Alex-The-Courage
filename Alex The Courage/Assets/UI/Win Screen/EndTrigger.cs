3413bfc6e2b063fd19abafbeeac81ff9cf45e094
Harryliow2001 <108907544+Harryliow2001@users.noreply.github.com>
2023-09-12 15:47:06 +0800

    victory animation

--- Alex The Courage/Assets/UI/Win Screen/EndTrigger.cs
+++ Alex The Courage/Assets/UI/Win Screen/EndTrigger.cs
@@ -1,15 +1,41 @@
+using System.Collections;
 using UnityEngine;
 
 public class EndTrigger : MonoBehaviour
 {
+    public bool isFinished;
+    public Animator animator;
     public GameManager gameManager;
 
-    private void OnTriggerEnter()
+    private void OnTriggerEnter(Collider other)
     {
-        gameManager.CompleteLevel();
+        if (other.gameObject.tag == "Player")
+        {
+            animator = other.GetComponent<Animator>();
+            if (animator != null)
+            {
+                gameManager.CompleteLevel();
+                animator.SetBool("IsFinished", true);
+                // Unlock the cursor and make it visible
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
 
-        // Unlock the cursor and make it visible
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+                // Start the coroutine to reset the parameter after 3 seconds
+                StartCoroutine(ResetIsFinishedAfterDelay());
+            }
+            else
+            {
+                Debug.LogError("Animator not found on the player!");
+            }
+        }
+    }
+
+    IEnumerator ResetIsFinishedAfterDelay()
+    {
+        // Wait for 3 seconds
+        yield return new WaitForSeconds(3f);
+
+        // Set IsFinished back to false
+        animator.SetBool("IsFinished", false);
     }
 }
