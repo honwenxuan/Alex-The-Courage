185b6fdaa6dd591406326adffb179b7a06c39190
honwenxuan <honwenxuan@gmail.com>
2023-09-13 18:15:53 +0800

    add select level UI

--- Alex The Courage/Assets/UI/MainMenu.cs
+++ Alex The Courage/Assets/UI/MainMenu.cs
@@ -8,6 +8,31 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void GoToLevel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoToLevel2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void GoToLevel3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void GoToLevel4()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void GoToLevel5()
+    {
+        SceneManager.LoadScene(5);
+    }
+
     public void QuitGame()
     {
         Debug.Log("Quit!");
